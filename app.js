fa513083ac28996afcf1abe3e841fd39a5f0c30b
Rasmus Falk <rf222fu@student.lnu.se>
2018-12-25 14:00:13 +0100

    Save database connection in app instance

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('./config/express');
+const database = require('./config/database');
 
 const app = express.run();
+const db = database.connect();
+
+app.set('db', db);
 
 app.use('/', require('./routes/index'));
 app.use('/add', require('./routes/add'));
