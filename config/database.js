000f83f6320bc424c71f4c203ae8e1b42fa80228
Rasmus Falk <rf222fu@student.lnu.se>
2018-12-25 13:59:15 +0100

    Create tables when connecting to database

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const sqlite3 = require('sqlite3').verbose();
+
+module.exports.connect = () => {
+  const db = new sqlite3.Database(process.env.DB_NAME);
+
+  db.serialize(() => {
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Albums (label CHAR, device CHAR, genre CHAR, albumTitle TEXT, band CHAR, year INT(4))'
+    );
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Tracks (name CHAR, length INT, albumTitle TEXT, band CHAR)'
+    );
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Producers (name CHAR, albumTitle TEXT, band CHAR)'
+    );
+  });
+
+  console.log(`Connected to database: ${process.env.DB_NAME}...`);
+
+  return db;
+};
