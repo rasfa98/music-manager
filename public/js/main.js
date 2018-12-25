9baad7e96a05aa650dee7b172284dea7c5b58284
Rasmus Falk <rf222fu@student.lnu.se>
2018-12-25 21:56:54 +0100

    Users will be able to dynamicly append tracks when adding an album

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-console.log('hello world!');
+const tracksSection = document.querySelector('#tracks');
+const tracksTable = document.querySelector('tbody');
+const trackRowTemplate = document.querySelector('#track-add-row');
+
+tracksSection.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (e.target.id === 'add-track') {
+    const newRow = document.importNode(trackRowTemplate.content, true);
+
+    tracksTable.appendChild(newRow);
+
+    const numberOfTracks = tracksTable.querySelectorAll('tr').length;
+
+    document.querySelectorAll('.track-nr')[
+      numberOfTracks - 1
+    ].textContent = numberOfTracks;
+  } else if (e.target.classList.contains('remove-track')) {
+    const numberOfTracks = tracksTable.querySelectorAll('tr').length;
+
+    if (numberOfTracks > 1) {
+      tracksTable.removeChild(e.target.closest('tr'));
+    }
+
+    const trackNumbers = tracks.querySelectorAll('.track-nr');
+
+    for (let i = 0; i < trackNumbers.length; i++) {
+      trackNumbers[i].textContent = i + 1;
+    }
+  }
+});
