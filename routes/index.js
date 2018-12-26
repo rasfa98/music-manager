4d182fc5995861334908daf236034692d304b737
Rasmus Falk <rf222fu@student.lnu.se>
2018-12-26 11:15:43 +0100

    List added albums on index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 
 router.route('/').get((req, res) => {
-  res.render('index');
+  const db = req.app.get('db');
+
+  db.all('SELECT * FROM Albums', (err, rows) => {
+    if (err) {
+      console.log(err);
+    }
+
+    res.render('index', { rows: rows });
+  });
 });
 
 module.exports = router;
