95f5dd3ed64f78cf120e98c9597a5415ec103452
Rasmus Falk <rf222fu@student.lnu.se>
2019-01-06 18:52:16 +0100

    Move creation of tables to another file

--- config/database.js
+++ config/database.js
@@ -1,50 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
+const databaseQueries = require('../lib/databaseQueries');
 
-module.exports.connect = () => {
+module.exports.connect = async () => {
   const db = new sqlite3.Database(process.env.DB_NAME);
 
-  db.serialize(() => {
-    db.run(
-      'CREATE TABLE IF NOT EXISTS Albums (label CHAR, device CHAR, genre CHAR, albumTitle TEXT, band CHAR, year INT(4), albumId CHAR)',
-      (db, err) => {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-    db.run(
-      'CREATE TABLE IF NOT EXISTS Tracks (trackName CHAR, trackLength INT, trackId CHAR)',
-      (db, err) => {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-    db.run(
-      'CREATE TABLE IF NOT EXISTS Producers (producerName CHAR, producerId CHAR)',
-      (db, err) => {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-    db.run(
-      'CREATE TABLE IF NOT EXISTS ProducedBy (albumId CHAR, producerId CHAR)',
-      (db, err) => {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-    db.run(
-      'CREATE TABLE IF NOT EXISTS MadeOf (albumId CHAR, trackId CHAR)',
-      (db, err) => {
-        if (err) {
-          console.log(err);
-        }
-      }
-    );
-  });
+  await databaseQueries.createTables(db);
 
   console.log(`Connected to database: ${process.env.DB_NAME}...`);
 
