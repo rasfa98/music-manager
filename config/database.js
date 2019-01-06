1305e1352d5fb7025b2b4454e90e2581d6d5771d
Rasmus Falk <rf222fu@student.lnu.se>
2019-01-06 19:14:02 +0100

    Revert change

--- config/database.js
+++ config/database.js
@@ -1,10 +1,55 @@
 const sqlite3 = require('sqlite3').verbose();
 const databaseQueries = require('../lib/databaseQueries');
 
-module.exports.connect = async () => {
+module.exports.connect = () => {
   const db = new sqlite3.Database(process.env.DB_NAME);
 
-  await databaseQueries.createTables(db);
+  db.serialize(() => {
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Albums (label CHAR, device CHAR, genre CHAR, albumTitle TEXT, band CHAR, year INT(4), albumId CHAR)',
+      (db, err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Tracks (trackName CHAR, trackLength INT, trackId CHAR)',
+      (db, err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    db.run(
+      'CREATE TABLE IF NOT EXISTS Producers (producerName CHAR, producerId CHAR)',
+      (db, err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    db.run(
+      'CREATE TABLE IF NOT EXISTS ProducedBy (albumId CHAR, producerId CHAR)',
+      (db, err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    db.run(
+      'CREATE TABLE IF NOT EXISTS MadeOf (albumId CHAR, trackId CHAR)',
+      (db, err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
 
   console.log(`Connected to database: ${process.env.DB_NAME}...`);
 
